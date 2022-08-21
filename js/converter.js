5bd40d04e43aa38c1054c800705c401921dffbef
MJ Brechner <mjbrechner@gmail.com>
2022-08-21 16:59:18 -0400

    added uppercase/lowecase converting

--- js/converter.js
+++ js/converter.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let textInput;
+let textUpperCase;
+let textLowerCase;
+
+
+function upperCase() {
+textInput = document.getElementById("original-text").value;
+textUpperCase = textInput.toUpperCase();
+document.getElementById("original-text").value = textUpperCase;
+}
+
+function lowerCase() {
+textInput = document.getElementById("original-text").value;
+textLowerCase = textInput.toLowerCase();
+document.getElementById("original-text").value = textLowerCase;
+}
