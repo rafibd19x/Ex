965dd3fc10dbf7f1d59c4db1ff15f462a06b94f9
MJ Brechner <mjbrechner@gmail.com>
2022-08-24 17:06:19 -0400

    text copied message

--- js/text-copy.js
+++ js/text-copy.js
@@ -2,7 +2,13 @@
 
 let textToCopy;
 
+function copiedReturnToNormal() {
+    document.getElementById("text-copied-message").style.visibility = "hidden";
+}
+
 function textCopier() {
     textToCopy = document.getElementById("original-text").value;
     navigator.clipboard.writeText(textToCopy);
+    document.getElementById("text-copied-message").style.visibility = "visible";
+    setTimeout(copiedReturnToNormal, 2000);
 }
