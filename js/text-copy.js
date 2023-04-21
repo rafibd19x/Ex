d34adfa34464110ab7c4f45c944ded919d47830d
MJ Brechner <mjbrechner@gmail.com>
2023-04-21 18:03:36 -0400

    added erase button & message when no text to copy

--- js/text-copy.js
+++ js/text-copy.js
@@ -1,14 +1,26 @@
 'use strict';
 
 let textToCopy;
+let textCopiedMessage = document.getElementById("text-copied-message");
 
 function copiedReturnToNormal() {
-    document.getElementById("text-copied-message").style.visibility = "hidden";
+    textCopiedMessage.style.visibility = "hidden";
 }
 
 function textCopier() {
     textToCopy = document.getElementById("original-text").value;
-    navigator.clipboard.writeText(textToCopy);
-    document.getElementById("text-copied-message").style.visibility = "visible";
+
+    if (textToCopy !== "") {
+        navigator.clipboard.writeText(textToCopy);
+        textCopiedMessage.innerText = "text copied!";
+    } else {
+        textCopiedMessage.innerText = "no text to copy";
+    }
+
+    textCopiedMessage.style.visibility = "visible";
     setTimeout(copiedReturnToNormal, 2000);
+}
+
+function textEraser() {
+    document.getElementById("original-text").value = "";
 }
