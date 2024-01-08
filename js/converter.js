6d8876469ea5c8b5337110e3c415df697ffd668b
rafibd19x <58140958+rafibd19x@users.noreply.github.com>
2024-01-08 23:49:06 +0600

    Update converter.js

--- js/converter.js
+++ js/converter.js
@@ -9,8 +9,11 @@ let textTitleCase;
 function upperCase() {
     textInput = document.getElementById("original-text").value;
     
-    const result = textInput.replace(/\./g, "€").replace(/\,/g, "¢").replace(/[a-z]/g, match => match+ '.').replace(/[A-Z]/g, match => match+ '.').replace(/[0-9]/g, match => match+ '.').replace(/B|b/g, ',chuto,b.').replace(/V|v/g, ',Boro,v.').replace(/N|n/g, ',chuto,n.').replace(/M|m/g, ',Boro,m.').replace(/P|p/g, ',opi,p.').replace(/E|e/g, ',2Ee,e.').replace(/O|o/g, ',2zo,o.').replace(/U|u/g, ',eou,2u.').replace(/S|s/g, ',yes,s.').replace(/R|r/g, ',aree,r.').replace(/ +/g, ",space.").replace(/\n/g, ',Newline,here.');
-             const resultt = result.replace(/\€/g, ",sentence,end.").replace(/\:/g, ',semicolon.').replace(/\¢/g, ",comma.");
+    const result = textInput.replace(/\./g, "€").replace(/\,/g, "¢").replace(/[a-z]/g, match => match+ '.').replace(/[A-Z]/g, match => match+ '.').replace(/[0-9]/g, match => match+ '.').replace(/B|b/g, 'ক').replace(/V|v/g, 'খ').replace(/N|n/g, 'গ').replace(/M|m/g, 'ঘ').replace(/P|p/g, 'ঙ').replace(/E|e/g, 'চ').replace(/O|o/g, 'ছ').replace(/U|u/g, 'জ').replace(/S|s/g, 'ঝ').replace(/R|r/g, 'ঞ').replace(/ +/g, ",space.").replace(/\n/g, ',Newline,here.');
+             
+
+const resultt = result.replace(/\€/g, ",sentence,end.").replace(/\:/g, ',semicolon.').replace(/\¢/g, ",comma.").replace(/\ক/g, ',chuto,b.').replace(/\খ/g, ',Boro,v.').replace(/\গ/g, ',chuto,n.').replace(/\ঘ/g, ',Boro,m.').replace(/\ঙ/g, ',opi,p.').replace(/\চ/g, ',2Ee,e.').replace(/\ছ/g, ',2zo,o.').replace(/\জ/g, ',eou,2u.').replace(/\ঝ/g, ',yes,s.').replace(/\ঞ/g, ',aree,r.');
+
 
     
     
